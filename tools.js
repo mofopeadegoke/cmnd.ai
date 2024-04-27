afeb0d899ff98d570ff81645e78ec8a5a6210607
mofopeadegoke <mofopeadegoke@gmail.com>
2024-04-27 09:56:19 +0300

    getAgentName tool being worked on

--- tools.js
+++ tools.js
@@ -8,6 +8,41 @@ const getProductSchema = yup.object({
   product: yup.string().label("product").required("should be a string"),
 });
 const getProductsJSONSchema = yupToJsonSchema(getProductSchema);
+
+// Defining the getAgentName Schema
+const getAgentNameSchema = yup.object({
+  propertyName: yup
+    .string()
+    .label("propertyName")
+    .required("should be a string"),
+});
+
+// JSON style of the getAgentName Schema
+const getAgentNameProductsJSONSchema = yupToJsonSchema(getAgentNameSchema);
+
+const getAgentName = {
+  name: "get_agent_name",
+  description: "Gets the agent that sold a property",
+  category: "hackathon",
+  subcategory: "backend",
+  functionType: "backend",
+  dangerous: false,
+  associatedCommands: [],
+  prerequisites: [],
+  parameters: getAgentNameSchema,
+  rerun: true,
+  rerunWithDifferentParameters: true,
+  runCmd: async ({ propertyName }) => {
+    try {
+      const { data } = await axios.post(`http://localhost:3000/getAgent`, {
+        propertyName,
+      });
+      return JSON.stringify(data);
+    } catch (err) {
+      return "Error trying to execute the tool";
+    }
+  },
+};
 const PRODUCT_FINDER = {
   name: "product_finder",
   description:
@@ -33,66 +68,66 @@ const PRODUCT_FINDER = {
   },
 };
 
-const weatherCitySchema = yup.object({
-  city: yup.string().label("city").required("should be a string"),
-});
-const weatherCityJSONSchema = yupToJsonSchema(weatherCitySchema);
-const WEATHER_FROM_LOCATION = {
-  name: "city_weather_data",
-  description: "gets the weather details from a given city name",
-  category: "hackathon",
-  subcategory: "communication",
-  functionType: "backend",
-  dangerous: false,
-  associatedCommands: [],
-  prerequisites: [],
-  parameters: weatherCityJSONSchema,
-  rerun: true,
-  rerunWithDifferentParameters: true,
-  runCmd: async ({ city }) => {
-    const ApiKey = process.env.WEATHER_API_KEY;
-    try {
-      const { data } = await axios.get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${ApiKey}`
-      );
-      return JSON.stringify({
-        weather: data.weather[0].description,
-        main: data.main,
-        coords: data.coord,
-      });
-    } catch (err) {
-      return "Error trying to execute the tool";
-    }
-  },
-};
+// const weatherCitySchema = yup.object({
+//   city: yup.string().label("city").required("should be a string"),
+// });
+// const weatherCityJSONSchema = yupToJsonSchema(weatherCitySchema);
+// const WEATHER_FROM_LOCATION = {
+//   name: "city_weather_data",
+//   description: "gets the weather details from a given city name",
+//   category: "hackathon",
+//   subcategory: "communication",
+//   functionType: "backend",
+//   dangerous: false,
+//   associatedCommands: [],
+//   prerequisites: [],
+//   parameters: weatherCityJSONSchema,
+//   rerun: true,
+//   rerunWithDifferentParameters: true,
+//   runCmd: async ({ city }) => {
+//     const ApiKey = process.env.WEATHER_API_KEY;
+//     try {
+//       const { data } = await axios.get(
+//         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${ApiKey}`
+//       );
+//       return JSON.stringify({
+//         weather: data.weather[0].description,
+//         main: data.main,
+//         coords: data.coord,
+//       });
+//     } catch (err) {
+//       return "Error trying to execute the tool";
+//     }
+//   },
+// };
 
-const getFilePathSchema = yup.object({
-  filePath: yup.string().label("filePath").required("should be a file path"),
-});
-const getFilePathJSONSchema = yupToJsonSchema(getFilePathSchema);
-const FILE_READER = {
-  name: "file_reader",
-  description:
-    "returns the contents of a file given its filepath in the repository",
-  category: "hackathon",
-  subcategory: "communication",
-  functionType: "backend",
-  dangerous: false,
-  associatedCommands: [],
-  prerequisites: [],
-  parameters: getFilePathJSONSchema,
-  rerun: true,
-  rerunWithDifferentParameters: true,
-  runCmd: async ({ filePath }) => {
-    try {
-      const buffer = fs.readFileSync(filePath);
-      const fileContents = buffer.toString("utf8");
-      return fileContents;
-    } catch (error) {
-      return "An error ocured while looking for the file content";
-    }
-  },
-};
+// const getFilePathSchema = yup.object({
+//   filePath: yup.string().label("filePath").required("should be a file path"),
+// });
+// const getFilePathJSONSchema = yupToJsonSchema(getFilePathSchema);
+// const FILE_READER = {
+//   name: "file_reader",
+//   description:
+//     "returns the contents of a file given its filepath in the repository",
+//   category: "hackathon",
+//   subcategory: "communication",
+//   functionType: "backend",
+//   dangerous: false,
+//   associatedCommands: [],
+//   prerequisites: [],
+//   parameters: getFilePathJSONSchema,
+//   rerun: true,
+//   rerunWithDifferentParameters: true,
+//   runCmd: async ({ filePath }) => {
+//     try {
+//       const buffer = fs.readFileSync(filePath);
+//       const fileContents = buffer.toString("utf8");
+//       return fileContents;
+//     } catch (error) {
+//       return "An error ocured while looking for the file content";
+//     }
+//   },
+// };
 
-const tools = [FILE_READER, PRODUCT_FINDER, WEATHER_FROM_LOCATION];
-module.exports = tools;
+// const tools = [FILE_READER, PRODUCT_FINDER, WEATHER_FROM_LOCATION];
+// module.exports = tools;
