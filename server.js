035ce739f53c1b852c9593102e944a4f3ea940ee
callmestiles <callmestiless@gmail.com>
2024-04-27 18:41:25 +0300

    added new endpoints and tools with mail sending functionality

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
 const pg = require("pg");
 const morgan = require("morgan");
 
@@ -17,15 +18,14 @@ const db = new pg.Client({
 
 db.connect();
 
-// app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
 app.use(morgan("tiny"));
 
 app.get("/", (req, res)=>{
     res.send("GROUP 4 HACAKATHON API");
 });
 
-//returns the agent name that sold a particular property
 app.post("/getAgent", async (req, res)=>{
     console.log(req.body);
     const propertyName = req.body.propertyName;
@@ -54,7 +54,6 @@ app.post("/getAgent", async (req, res)=>{
 });
 
 
-//returns the names of the properties that has been sold
 app.get("/getSold", async (req, res) => {
     try {
         const soldPropertiesResult = await db.query("SELECT * FROM sold_properties JOIN properties ON properties.id = property_id JOIN customers ON customers.id = customer_id JOIN agents ON agents.id = agent_id");
@@ -88,7 +87,6 @@ app.get("/getTotalNum", async (req, res)=>{
 });
 
 
-//returns the number of properties that has been sold
 app.get("/getNumSold", async (req, res)=>{
     try{
         const result = await db.query("SELECT COUNT(DISTINCT property_id) AS total_properties_sold FROM sold_properties");
@@ -99,42 +97,6 @@ app.get("/getNumSold", async (req, res)=>{
     }
 });
 
-app.get("/getNumRem", async (req, res)=>{
-    try{
-        const result = await db.query("SELECT COUNT(DISTINCT id) AS total_properties FROM properties");
-        console.log(result);
-        res.send(200);
-    }catch(err){
-        console.log(err);
-        res.json(err);
-    }
-})
-//returns the prices of properties that has been sold
-app.post("/getPrice", async(req, res)=>{
-    try{
-        const propertyName = req.body.propertyName;
-        console.log(propertyName);
-        try{
-            const result = await db.query("SELECT id from properties WHERE name = $1", [propertyName]);
-            const propertyId = result.rows[0].id;
-            console.log(propertyId);
-            try{
-                const result = await db.query("SELECT amount FROM sold_properties WHERE property_id = $1", [propertyId]);
-                const amount = result.rows[0].amount;
-                res.json(amount).status(200);
-            }catch(err){
-                console.log(err);
-                res.json("Property hasn't been sold yet").statusCode(404);
-            }
-        }catch(err){
-            console.log(err);
-            res.json("No such property");
-        }
-    }catch(err){
-        console.log(err);
-        res.status(500).json("Property hasn't been bought yet");
-    }
-});
 
 app.get("/topCustomers", async (req,res)=>{
     try{
@@ -145,7 +107,41 @@ app.get("/topCustomers", async (req,res)=>{
         console.log(err);
         res.send(500);
     }
-})
+});
+
+app.post("/sendThanks", (req, res)=>{
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'dunamisolukayode@gmail.com',
+            pass: 'zukh tntf zasq oqxb'
+        }
+    });
+    
+    const mailOptions = {
+        from: 'dunamisolukayode@gmail.com',
+        to: `${req.body.customer_email}`,
+        subject: 'Thank you for your patronage 🥳🚀',
+        text: `
+            Dear ${req.body.customer_name},
+
+            We extend our deepest gratitude for choosing Dovec Ventures for your recent purchase of ${req.body.propertyName}. We are truly honored to have been part of this journey with you. At Dovec Ventures, our commitment to exceptional service is unwavering, and your trust in our team reinforces our dedication to excellence. We are confident that your new property will bring you joy, comfort, and prosperity for years to come, and we remain at your service for any assistance you may require in the future. Thank you for choosing Dovec Ventures.
+
+            Warm regards,
+            Team 4
+            Dovec Ventures
+        `
+    };
+    
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent successfully:', info.response);
+        }
+    });
+    res.send(200);
+});
 
 
 app.listen(port, () => {
